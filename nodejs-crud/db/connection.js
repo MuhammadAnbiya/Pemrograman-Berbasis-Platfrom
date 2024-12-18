05023e7bec8df200d201b322f43afde770658c55
muhammadanbiya <muhammadanbiya@localhost.localdomain>
2024-12-19 00:01:39 +0700

    PBP - Tugas 9 create CRUD

---
+++ nodejs-crud/db/connection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const db = mysql.createPool({
+    host: '127.0.0.1', // Gunakan IPv4
+    user: 'root',
+    password: 'user', // Ganti dengan password root Anda
+    database: 'mahasiswa_db'
+  });
+  
+
+db.getConnection((err, connection) => {
+
+if (err) {
+console.error('Database connection failed:', err.message);
+} else {
+console.log('Connected to database.');
+connection.release();
+}
+});
+
+module.exports = db;
